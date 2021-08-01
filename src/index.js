05c09079a3e77c95c23d3accf851a061f6f2a8f8
ngotanan147 <ngotanan147@gmail.com>
2021-08-02 01:21:53 +0700

    commit 2

--- src/index.js
+++ src/index.js
@@ -26,3 +26,4 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
+// "start": "nodemon --inspect src\\index.js",
